90c6915a77f7cce93a0f58ce9b0daa36ba1ecf88
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-08-14 10:14:30 +0700

    Update gelex_arduino.ino

--- gelex_arduino/gelex_arduino.ino
+++ gelex_arduino/gelex_arduino.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <SD.h>
+#include <RTClib.h>
+
+char daysOfTheWeek[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
 
 #define RST_PIN 9
 #define SS_PIN 10
@@ -8,15 +11,20 @@
 #define BUZZER_PIN 4
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-
 MFRC522::MIFARE_Key key;
-
+RTC_DS3231 rtc;
 File file;
+bool isOpen = false;
 
 void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
   SPI.begin();
   mfrc522.PCD_Init();
   SD.begin(CS_PIN);
+  if (! rtc.begin()) {
+    tone(BUZZER_PIN, 500);
+    while (1);
+  }
   key.keyByte[0] = 0xA0;
   key.keyByte[1] = 0xA1;
   key.keyByte[2] = 0xA2;
@@ -26,6 +34,7 @@ void setup() {
 }
 
 void loop() {
+  if (isOpen)file.close();
   noTone(BUZZER_PIN);
   if ( ! mfrc522.PICC_IsNewCardPresent())
     return;
@@ -35,54 +44,71 @@ void loop() {
   MFRC522::StatusCode status;
   byte buffer[18];
   byte size = sizeof(buffer);
-  
+
+  DateTime now = rtc.now();
+
   status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 11, &key, &(mfrc522.uid));
   if (status != MFRC522::STATUS_OK) {
     tone(BUZZER_PIN, 500);
-    delay(5000);
+    delay(2000);
     return;
   }
-  
+
   status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(9, buffer, &size);
   if (status != MFRC522::STATUS_OK) {
     tone(BUZZER_PIN, 500);
-    delay(5000);
+    delay(2000);
     return;
   }
 
   file = SD.open("data.txt", FILE_WRITE);
+  isOpen = true;
 
-  for(int i = 0; i < 16; i++){
+  for (int i = 0; i < 16; i++) {
     file.print((char)buffer[i]);
   }
   file.println();
-  
+
   status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 27, &key, &(mfrc522.uid));
   if (status != MFRC522::STATUS_OK) {
     tone(BUZZER_PIN, 500);
-    delay(5000);
+    delay(2000);
     return;
   }
 
   status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(24, buffer, &size);
   if (status != MFRC522::STATUS_OK) {
     tone(BUZZER_PIN, 500);
-    delay(5000);
+    delay(2000);
     return;
   }
 
-  for(int i = 0; i < 8; i++){
+  for (int i = 0; i < 8; i++) {
     file.print(String(buffer[i], HEX));
   }
   file.println();
 
-  file.println(millis());
+  file.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  file.print(", ");
+  file.print(now.day(), DEC);
+  file.print('/');
+  file.print(now.month(), DEC);
+  file.print('/');
+  file.print(now.year(), DEC);
+  file.print(' ');
+  file.print(now.hour(), DEC);
+  file.print(':');
+  file.print(now.minute(), DEC);
+  file.print(':');
+  file.print(now.second(), DEC);
 
+  file.println();
   file.println("$");
-  
+
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
   file.close();
+  isOpen = false;
   tone(BUZZER_PIN, 3000);
   delay(250);
 }
